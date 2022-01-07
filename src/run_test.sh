51d9bbc856e55e88b071db0bc94717103bb0d80e
Deviance-Project <93658342+Deviance-Project@users.noreply.github.com>
2022-01-07 19:53:03 +0900

    Delete src directory

--- src/run_test.sh
+++
@@ -1,7 +0,0 @@
-export CUDA_VISIBLE_DEVICES=0
-
-python main.py \
---train_folder_directory ./data --SEA_folder_directory ./application --DIA_folder_directory ./data \
---experiment_description [AAAI2022]application --batch_size 1 --num_threads 8 \
---classifier_type SEA_DIA --superpoint_type con --test_only --test_metric SEA \
---weight_load_pth ./weight_file/[AAAI2022-DevianceNet]SEA.pth
